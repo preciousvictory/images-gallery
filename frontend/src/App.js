33246907c876695eb5f1c0a490cd3d2d319c4c95
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-07-20 22:41:50 +0100

    Changed favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Images-Gallery</h1>
+        <h1>Images Gallery</h1>
       </header>
     </div>
   );
